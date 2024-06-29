08ce0dd50288814c305069b4027103624e98424c
keshav_regmi <keshavregmi70@gmail.com>
2024-06-29 17:22:47 +0530

    Added  Styles

---
+++ src/styles/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  listItem: {
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    width: "100%",
+  },
+});
